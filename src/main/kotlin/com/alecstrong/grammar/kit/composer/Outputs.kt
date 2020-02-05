32b720671b25ba3d9fcee936ac47a6003f387f28
Alec Strong <astrong@squareup.com>
2020-02-04 23:41:05 -0500

    Initial Commit

---
+++ src/main/kotlin/com/alecstrong/grammar/kit/composer/Outputs.kt
@@ -0,0 +1,32 @@
+package com.alecstrong.grammar.kit.composer
+
+import com.squareup.kotlinpoet.ClassName
+import java.io.File
+
+
+
+internal data class Outputs(
+  val outputFile: File,
+  val psiPackage: String,
+  val parserClass: ClassName,
+  val outputPackage: String,
+  val outputDirectory: File
+)
+
+internal fun getOutputs(
+  outputDirectory: File,
+  bnf: File,
+  root: File
+): Outputs {
+  val outputPackage = bnf.parentFile.toRelativeString(root).replace('/', '.')
+  fun outputDirectory(): File = File(outputDirectory, outputPackage.replace('.', '/'))
+  val outputBnf = File(outputDirectory().path, bnf.name)
+
+  return Outputs(
+      outputFile = outputBnf,
+      psiPackage = "${outputPackage}.psi",
+      outputPackage = outputPackage,
+      parserClass = ClassName(outputPackage, "${bnf.nameWithoutExtension.capitalize()}Parser"),
+      outputDirectory = outputDirectory()
+  )
+}
