2340edd8aa8feccb98a39c9f043192c2ed5c099a
hfhbd <hfhbd@users.noreply.github.com>
2022-12-09 14:04:46 +0100

    Fix stupid applying test

---
+++ src/test/fixtures/applying/src/main/kotlin/com/example/FooElementType.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import com.intellij.lang.*
+import com.intellij.psi.tree.*
+
+internal class FooElementType(debugName: String) : IElementType(debugName, FooLanguage)
+
+object FooLanguage : Language("Foo")
+
+internal class FooTokenType(debugName: String) : IElementType(debugName, FooLanguage) {
+    override fun toString(): String = "FooTokenType." + super.toString()
+}
